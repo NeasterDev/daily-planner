95ea29f3399f44c1516786f22b21c3a25396578e
Nicholas Easter <nicholas.easter17@gmail.com>
2021-12-15 10:50:42 -0800

    finished basic html template for time block

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,8 +1,10 @@
 const dateTime = luxon.DateTime.local();
 // Entire date with name [change DATA_HUGE for different format]
 var readableDate = dateTime.toLocaleString(luxon.DateTime.DATE_HUGE);
+// using JQuery, select the currentDay element
 var readableDateEl = $('#currentDay');
 // Gets the current day
 var day = dateTime.toFormat('EEEE');
 
 readableDateEl.text(readableDate) ;
+
