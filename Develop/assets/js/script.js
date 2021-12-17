420081936a8afe09a13a858671a12bb500bb6ebc
Nicholas Easter <nicholas.easter17@gmail.com>
2021-12-17 11:52:51 -0800

    finished time blocks

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,7 +2,8 @@
 const dateTime = luxon.DateTime.local();
 var readableDate = dateTime.toLocaleString(luxon.DateTime.DATE_HUGE); // Entire date with name [change DATA_HUGE for different format]
 var day = dateTime.toFormat('EEEE'); // Gets the current day
-
+var hour = dateTime.hour;
+console.log(hour);
 // JQuery variables
 var readableDateEl = $('#currentDay');
 var containerEl = $('.container');
@@ -10,24 +11,10 @@ var containerEl = $('.container');
 // Set date element to the current date
 readableDateEl.text(readableDate);
 
-// var containerClickHandler = function (event) {
-//     var target = $(event.target);
-//     if (target.is(".description")) {
-//         target.append("<textarea></textarea>");
-//     }
-// }
-
-// var containerBlue = function(event) {
-//     var target = $(event.target);
-
-// }
-
-// $(containerEl).on('click', containerClickHandler);
-// $(containerEl).on('blur', containerBlur);
-
 $('.description').on("click",  function() {
     var textInput = $('<textarea>');
-    $(this).append(textInput);
+    $(this).replaceWith(textInput);
 
     textInput.trigger("focus");
-})
+});
+
